3280c360d328718a0dd256cd4c727438b42e1ff5
RedIsGaming <jaredhofman@protonmail.com>
2024-10-27 00:07:45 +0200

    update: It should work now lol

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import("tailwindcss").Config} */
 export default {
-  content: ["./src/**/*.{html,js,rs,css}", "index.html"],
+  content: ["./src/**/*.{html,js,rs}", "index.html"],
   theme: {
     extend: {
       fontFamily: {
